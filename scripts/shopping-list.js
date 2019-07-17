a517c7572cf33e22e3331c2ce6f5bf7240ed2116
Julianb12 <julianb1212@gmail.com>
2019-07-17 11:13:59 -0500

    shopping list uses Item.create method

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -56,7 +56,15 @@ const shoppingList = (function(){
   
   
   function addItemToShoppingList(itemName) {
-    store.items.push({ id: cuid(), name: itemName, checked: false });
+    try {
+      Item.validateName(itemName);
+      const newItemName = Item.create(itemName);
+      store.items.push(newItemName);
+      //render(); 
+    } catch (error) {
+      console.log(`Cannot add item: ${error.message}`);
+      
+    }
   }
   
   function handleNewItemSubmit() {
