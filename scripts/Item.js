517ad49bc4ba2ab64d88e390287044a8f08a82fe
Sam Walker <jack.nasal@gmail.com>
2019-07-17 12:01:59 -0400

    wrote code for Items.js

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,7 +1,22 @@
 'use strict';
 
 const Item = (function(){
+  function validateName(name) {
+    if (!name) {
+      throw new TypeError('Name does not exist.');
+    }
+  }
+  function create(name) {
+    return {
+      id: cuid(),
+      name,
+      checked: false
+    };
+  }
   
-
+  return {
+    validateName,
+    create
+  };
 
 }());
